2d2fbc54f57bb2e9ae442338e0d0a622703812f2
VYSY0 <263145403+VYSY0@users.noreply.github.com>
2026-02-23 13:35:35 +0100

    asfawf

--- scripts/popUp.js
+++ scripts/popUp.js
@@ -24,17 +24,33 @@ const projectData = {
   races: `
     <h1>Races & More</h1>
     <hr>
-    <p>System ras i mechanik RPG, pozwalający graczom tworzyć unikalne postacie.</p>
+    <p>Placeholder</p>
   `,
   endfallen: `
     <h1>EndfallenSMP</h1>
     <hr>
-    <p>Mroczny projekt z własnym lore i mechaniką przetrwania w świecie Endfallen.</p>
+    <p>Placeholder</p>
   `,
   bread: `
     <h1>BreadSMP</h1>
     <hr>
-    <p>Projekt społecznościowy z twistem, skupiony na współpracy i kreatywności graczy.</p>
+    <p id="small-info">Server Java. Existed in time between: 2020-2023</p>
+    <p>BreadSMP was a Java Server created by VYSY0, in that time he was known as VToastyToast.<br>
+    It originated from the idea of private YouTuber's SMPs ideas.</p>
+    <hr>
+    <h1>How It Started</h1>
+    <p>BreadSMP started as a normal friend's only server. It had around 5-6 members online per day.
+    <br>
+    <br>
+    <p>At around 2021, after one member left due to private reasons, VYSY0 started to wonder if it would be a great idea to transfer the server from Free Hosting, to payed Hosting.. And publish it as a public whitelist-only server accessible by everyone.</p>
+    <hr>
+    <h1>Early Seasons</h1>
+    <p>First seasons of BreadSMP had no lore, it was online only to have fun with random members.<br><br>
+    On Season 2 one member joined, and it started the cycle of seasons with lore. First it was an catastrophe, then an entity from nether. Finally, secret laboratories..</p>
+    <hr>
+    <h1>The End..</h1>
+    <p>On season 5, BreadSMP was sadly announced to be discontinued due to PC problems.<br><br>
+
   `
 };
 
