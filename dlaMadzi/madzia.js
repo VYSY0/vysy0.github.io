f42b77f6d10948602b809f0371450e90f082cd7f
SamuraiBread <vysthevysy0@gmail.com>
2026-07-01 13:15:42 +0200

    Update for maddie

--- dlaMadzi/madzia.js
+++ dlaMadzi/madzia.js
@@ -2,12 +2,47 @@ function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+let Page1buttonIsActive = false
+
 const background = document.getElementById('bg');
-const sekcja = document.getElementById(`sekcjaOpa`);
-const header = document.getElementById(`headerL`);
+const header = document.getElementById('headerView')
+
+const  buttonSection = document.getElementById('buttonSection')
+
 window.addEventListener('load', async () => {
   background.style.filter = 'blur(0)';
-  await wait(5000);
-  sekcja.style.opacity = 1;
-  header.style.opacity = 1;
+  background.style.width = '100%'
+  await wait(5000)
+
+  header.style.transform = "translateY(0)"
+  header.style.opacity = 1
+  await wait(1000)
+  buttonSection.style.opacity = 1
+  Page1buttonIsActive = true
 });
+
+
+const buttonYes = document.getElementById('buttonYes')
+const buttonNo = document.getElementById(`buttonNo`)
+
+const message = document.getElementById(`message`)
+
+
+buttonNo.addEventListener(`click`, async () => {
+  if (!Page1buttonIsActive) return;
+  const answer = confirm(`Napewno nie chcesz otworzyć prezentu?`)
+
+  if (answer) {
+  window.close()
+}
+})
+
+buttonYes.addEventListener(`click`, async () => {
+    if (!Page1buttonIsActive) return;
+
+  buttonSection.style.opacity = 0
+  buttonNo.style.cursor = "auto"
+  buttonYes.style.cursor = "auto"
+  message.style.opacity = 1
+  Page1buttonIsActive = false
+})
